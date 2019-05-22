e0c6b21214d04bdd2b61111125b5ea94b487439c
megfan <megfa@yahoo.com>
2019-05-22 18:28:13 +0200

    Updates

---
+++ precache-manifest.cf4208f633c848aed973bdf0352a22c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a02f421cb282cc4c47029133c3da17",
+    "url": "/SPA_with_REACT-REDUX/bild/static/media/1.24a02f42.jpg"
+  },
+  {
+    "revision": "df10ac00188bcac85d80",
+    "url": "/SPA_with_REACT-REDUX/bild/static/js/runtime~main.1fee69f1.js"
+  },
+  {
+    "revision": "2eb6cfb89496900544f8",
+    "url": "/SPA_with_REACT-REDUX/bild/static/js/main.73c24f0f.chunk.js"
+  },
+  {
+    "revision": "a012cbdf936fd3d44a6a",
+    "url": "/SPA_with_REACT-REDUX/bild/static/js/2.95675c2a.chunk.js"
+  },
+  {
+    "revision": "2eb6cfb89496900544f8",
+    "url": "/SPA_with_REACT-REDUX/bild/static/css/main.5b263357.chunk.css"
+  },
+  {
+    "revision": "b3cde9d439224d2776bc071f29d3a948",
+    "url": "/SPA_with_REACT-REDUX/bild/index.html"
+  }
+];
