a42f49bfaa2a0e948003cef58bc8678becbeaa95
megfan <megfa@yahoo.com>
2019-06-19 10:28:04 +0200

    css modyfication - in progress

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -6,14 +6,14 @@ class Items extends Component {
     return (this.props.items.map(item => {
       return (item.map((elem, index) => {
         return(
-          <div className="app__results__container--item" key={index}>
-              <img src={elem.links[0].href} alt=""/>
+          <div className="app__results__container--item" key={index}>             
               <div className="app__results__container--item--data">
                 <h2>{elem.data[0].title}</h2>
                 <h3>{elem.data[0].date_created}</h3>
                 <p>
                 {elem.data[0].description.length > 600 ? elem.data[0].description.substring(0,600) + '...' : null}</p>
               </div>              
+              <img src={elem.links[0].href} alt=""/>
           </div>
         )
       }));
@@ -23,7 +23,6 @@ class Items extends Component {
     const { items } = this.props.items;
     return (
       <div className="app__results">
-        <h1 className="app__results--title">Your results:</h1>
         <div className="app__results__container">
           {this.props.items === [] || this.props.items === undefined ? null : this.renderList()}
         </div>
--- src/store/actions/itemActions.js
+++ src/store/actions/itemActions.js
@@ -4,7 +4,7 @@ export const searchResult = search => async dispatch => {
   const response = await axios.get(`https://images-api.nasa.gov/search?q=${search}&media_type=image`);
   dispatch({
     type: 'SEARCH_RESULT',
-    payload: response.data.collection.items.slice(0,5)
+    payload: response.data.collection.items.slice(0,20)
   })
 };
 
